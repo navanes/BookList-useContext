1db7e6334fa5bd9cc88d12548a212d261a00c259
navanes <mr.narek.avanesian@gmail.com>
2023-02-14 16:22:11 +0400

    Add the option to read the files from localstorage for the initial values

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,4 +1,4 @@
-import React, {createContext, useReducer} from 'react';
+import React, {createContext, useEffect, useReducer} from 'react';
 import {bookReducer} from "../reducers/bookReducer";
 
 /*{title: 'Lord Of The Rings', author: 'J. R. R. Tolkien', id: 1},
@@ -8,8 +8,13 @@ import {bookReducer} from "../reducers/bookReducer";
 export const BookContext = createContext();
 
 function BookContextProvider({children}) {
-    const [books, dispatch] = useReducer(bookReducer,[])
-
+    const [books, dispatch] = useReducer(bookReducer,[], ()=> {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    })
+    useEffect(()=> {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books])
     return (
         <BookContext.Provider value={{books, dispatch}}>
             {children}
